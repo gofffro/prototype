b78c6d8cd0aee8900c82adf253e97bac4ffc9b98
gofffro <vazheninstepan@gmail.com>
2025-06-27 11:18:59 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace prototype
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Person original = new Person("Иван", 30);
+      Person clone = (Person)original.Clone();
+
+      clone.Name = "Степан";
+
+      Console.WriteLine("Оригинал: " + original.Name + ", возраст: " + original.Age);
+      Console.WriteLine("Клон: " + clone.Name + ", возраст: " + clone.Age);
+    }
+  }
+
+  public class Person : ICloneable
+  {
+    public string Name;
+    public int Age;
+
+    public Person(string name, int age)
+    {
+      Name = name;
+      Age = age;
+    }
+
+    public object Clone()
+    {
+      return this.MemberwiseClone();
+    }
+  }
+}
